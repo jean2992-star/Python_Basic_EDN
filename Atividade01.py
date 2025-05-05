ffd311da1016b1f9fb1db0382bd9abf6fdc59964
JeanIvelsonne <jeanivelsonne29@gmail.com>
2025-05-05 10:35:15 -0300

    A1

---
+++ Atividade01.py
@@ -0,0 +1,25 @@
+# Atividade 01 
+print("\nOlá, Mundo!\n")
+# Atividade 02 -soma 
+numero1 = 12
+numero2 = 14
+soma = numero1 + numero2
+print("A soma de", numero1, "+", numero2, " = ", soma)
+# Atividade 03 - Calcular volume 
+
+Comprimento = 12 
+Largura = 14 
+Altura = 20 
+Volume = Comprimento * Largura * Altura
+print(f"\nO volume é {Volume} cm³\n")
+# Atividade 04 - Calcular de Preço Total
+
+produto = "Cadeira Infantil"
+preco =  12.40
+quantidade = 3 
+
+total = preco * quantidade
+
+print("Nome do produto: Cadeira Infantil\nPreço unitário: R$ 12.40\nQuantidade: 3\nPreço total: R$ 37.20")
+
+
